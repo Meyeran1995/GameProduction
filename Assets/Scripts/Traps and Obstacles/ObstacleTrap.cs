f397f9851959ea395d34a5028ded30a6657fae12
André Meyer <meyeran@uni-bremen.de>
2021-05-04 16:12:20 +0200

    BEGIN Work on screenbound snapping

--- Assets/Scripts/Traps and Obstacles/ObstacleTrap.cs
+++ Assets/Scripts/Traps and Obstacles/ObstacleTrap.cs
@@ -13,8 +13,31 @@ public class ObstacleTrap : ATrap
     protected override void TriggerTrap(Collider2D collision)
     {
         if (!collision.gameObject.CompareTag("Player")) return;
-        
+
         obstacleBody.gameObject.SetActive(true);
+        SnapToScreenBounds(true, true);
         obstacleBody.bodyType = RigidbodyType2D.Dynamic;
+    }
+
+    protected virtual void SnapToScreenBounds(bool matchX, bool matchY)
+    {
+        if(!matchX && !matchY) return;
+
+        Vector2 newPos = obstacleBody.transform.position;
+        Vector2 direction = (obstacleBody.transform.position - transform.position).normalized;
+
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, direction, Mathf.Infinity, 1 << 11);
+
+        if (matchX)
+        {
+           newPos.x = hit.point.x;
+        }
+
+        if (matchY)
+        {
+            newPos.y = hit.point.y;
+        }
+
+        obstacleBody.transform.position = newPos;
     }
 }
--- Assets/Scripts/Traps and Obstacles/RollingObstacleTrap.cs
+++ Assets/Scripts/Traps and Obstacles/RollingObstacleTrap.cs
@@ -5,8 +5,35 @@ public class RollingObstacleTrap : ObstacleTrap
     protected override void TriggerTrap(Collider2D collision)
     {
         if (!collision.gameObject.CompareTag("Player")) return;
+
         obstacleBody.gameObject.SetActive(true);
+        SnapToScreenBounds(true, true);
         obstacleBody.bodyType = RigidbodyType2D.Dynamic;
         obstacleBody.transform.GetComponent<AMovingObstacle>().MovementDir = (collision.transform.position - obstacleBody.transform.position).normalized;
+    }
+
+    protected override void SnapToScreenBounds(bool matchX, bool matchY)
+    {
+        if (!matchX && !matchY) return;
+
+        Vector2 newPos = obstacleBody.transform.position;
+        Vector2 direction = (obstacleBody.transform.position - transform.position).normalized;
+
+        RaycastHit2D hitScreenEdge = Physics2D.Raycast(obstacleBody.transform.position, obstacleBody.transform.right, Mathf.Infinity, 1 << 11 | 1 << 8);
+        //RaycastHit2D hitTerrain = Physics2D.Raycast(obstacleBody.transform.position, obstacleBody.transform.right, Mathf.Infinity, 1 << 11 | 1 << 8);
+
+        if (matchX)
+        {
+            newPos.x = hitScreenEdge.point.x;
+            //if (hitTerrain.transform.gameObject.layer == 8)
+            //    newPos.y = hitTerrain.point.y;
+        }
+
+        if (matchY)
+        {
+            newPos.y = hitScreenEdge.point.y;
+        }
+
+        obstacleBody.transform.position = newPos;
     }
 }
