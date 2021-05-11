9f12c4d8c440c0afe3c97fa8d1586671290093ec
André Meyer <meyeran@uni-bremen.de>
2021-05-11 17:46:26 +0200

    REWORK Delayed following

--- Assets/Scripts/Characters/BubbleCharacter.cs
+++ Assets/Scripts/Characters/BubbleCharacter.cs
@@ -9,7 +9,6 @@ public class BubbleCharacter : MonoBehaviour
 
     [SerializeField] [Tooltip("With how much delay is the character following the mouse?")] [Range(0f, 0.5f)] private float movementDelay;
     private const int MAX_FPS = 60;
-
     private Vector2[] positionBuffer;
     private float[] timeBuffer;
     private int oldestIndex;
@@ -28,7 +27,7 @@ public class BubbleCharacter : MonoBehaviour
         positionBuffer = new Vector2[bufferLength];
         timeBuffer = new float[bufferLength];
 
-        positionBuffer[0] = positionBuffer[1] = transform.position;
+        positionBuffer[0] = positionBuffer[1] = mainCam.WorldToScreenPoint(transform.position);
         timeBuffer[0] = timeBuffer[1] = Time.time;
 
         oldestIndex = 0;
@@ -43,13 +42,18 @@ public class BubbleCharacter : MonoBehaviour
 
     #region Delayed movement
 
-    //Delay code taken from:
+    //Delay code adapted from:
     //https://gamedev.stackexchange.com/questions/120189/unity-method-to-get-a-vector-which-is-x-seconds-behind-another-moving-vector-a/120225#120225
 
+    /// <summary>
+    /// Tries to add a new position to the cache
+    /// </summary>
+    /// <param name="mousePos"></param>
     private void AddNewPosToCache(Vector2 mousePos)
     {
         // Insert newest position into our cache.
         // If the cache is full, overwrite the latest sample.
+
         int newIndex = (newestIndex + 1) % positionBuffer.Length;
         if (newIndex != oldestIndex)
             newestIndex = newIndex;
@@ -58,13 +62,34 @@ public class BubbleCharacter : MonoBehaviour
         timeBuffer[newestIndex] = Time.time;
     }
 
+    /// <summary>
+    /// Moves to the next position, according to delayed targeted time
+    /// </summary>
     private void MoveToNextPos()
     {
         // Skip ahead in the buffer to the segment containing our target time.
         float targetTime = Time.time - movementDelay;
-        int nextIndex;
-        while (timeBuffer[nextIndex = (oldestIndex + 1) % timeBuffer.Length] < targetTime)
+        int nextIndex = (oldestIndex + 1) % timeBuffer.Length;
+
+#if UNITY_EDITOR
+
+        int startingIndex = nextIndex;
+
+        while (timeBuffer[nextIndex] < targetTime)
+        {
             oldestIndex = nextIndex;
+            nextIndex = (oldestIndex + 1) % timeBuffer.Length;
+            if (nextIndex != startingIndex) continue;
+            Debug.LogError("No suitable target time found, breaking out of while loop");
+            return;
+        }
+#else
+        while (timeBuffer[nextIndex] < targetTime)
+        {
+            oldestIndex = nextIndex;
+            nextIndex = (oldestIndex + 1) % timeBuffer.Length;
+        }
+#endif
 
         // Interpolate between the two samples on either side of our target time.
         float span = timeBuffer[nextIndex] - timeBuffer[oldestIndex];
@@ -74,26 +99,9 @@ public class BubbleCharacter : MonoBehaviour
             progress = (targetTime - timeBuffer[oldestIndex]) / span;
         }
 
+        //float progress = (targetTime - timeBuffer[oldestIndex]) / span;
         bubbleBody.MovePosition(mainCam.ScreenToWorldPoint(Vector3.Lerp(positionBuffer[oldestIndex], positionBuffer[nextIndex], progress)));
     }
 
-    //private void OnDrawGizmos()
-    //{
-    //    if (_positionBuffer == null || _positionBuffer.Length == 0)
-    //        return;
-
-    //    Gizmos.color = Color.grey;
-
-    //    Vector3 oldPosition = _positionBuffer[_oldestIndex];
-    //    int next;
-    //    for (int i = _oldestIndex; i != _newestIndex; i = next)
-    //    {
-    //        next = (i + 1) % _positionBuffer.Length;
-    //        Vector3 newPosition = _positionBuffer[next];
-    //        Gizmos.DrawLine(oldPosition, newPosition);
-    //        oldPosition = newPosition;
-    //    }
-    //}
-
-    #endregion
+#endregion
 }
