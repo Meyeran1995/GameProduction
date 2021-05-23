99529fc3207e536ab52f6c6114539b71aaad17f9
André Meyer <meyeran@uni-bremen.de>
2021-05-23 13:59:21 +0200

    Save

---
+++ Assets/Scripts/Audio/CollisionSoundController.cs
@@ -0,0 +1,9 @@
+using JetBrains.Annotations;
+using UnityEngine;
+
+public class CollisionSoundController : MonoBehaviour
+{
+    public static void UnmuteCollisions() => FMODUnity.RuntimeManager.StudioSystem.setParameterByName("MuteCollisions", 0);
+
+    public static void MuteCollisions() => FMODUnity.RuntimeManager.StudioSystem.setParameterByName("MuteCollisions", 1);
+}
--- Assets/Scripts/Audio/MuteCollisions.cs
+++
@@ -1,8 +0,0 @@
-using JetBrains.Annotations;
-using UnityEngine;
-
-public class MuteCollisions : MonoBehaviour
-{
-    [UsedImplicitly]
-    public void UnmuteCollisions() => FMODUnity.RuntimeManager.StudioSystem.setParameterByName("MuteCollisions", 0);
-}
